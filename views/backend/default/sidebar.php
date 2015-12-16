970cd2000c08e5dd59eb4883012213cd812085b7
Mutasim Ridlo, S.Kom <ridho@mutasimridlo.com>
2015-12-16 12:44:20 +0700

    modify sidebar style

--- views/backend/default/sidebar.php
+++ views/backend/default/sidebar.php
@@ -1,27 +1,89 @@
 <div class="col-md-3">
     <div class="sidebarpadding">
-    <ul class="list-group">
-      <li class="list-group-item list-group-item-info"><strong>Home</strong></li>
-      <li class="list-group-item"><a href="">Dashboard</a></li>
-      <li class="list-group-item"><a href="">Preferences</a></li>
-      <li class="list-group-item list-group-item-info"><a href="">Forums</a></li>
-      <li class="list-group-item"><a href="">All Forums</a></li>
-      <li class="list-group-item"><a href="">Add Forum</a></li>
-      <li class="list-group-item list-group-item-info"><a href="">Users</a></li>
-      <li class="list-group-item"><a href="">All Users</a></li>
-      <li class="list-group-item"><a href="">Add User</a></li>
-      <li class="list-group-item list-group-item-info"><a href="">User Groups</a></li>
-      <li class="list-group-item"><a href="">All Groups</a></li>
-      <li class="list-group-item"><a href="">Add Group</a></li>
-      <li class="list-group-item list-group-item-info"><a href="">Themes</a></li>
-      <li class="list-group-item"><a href="">Themes</a></li>
-      <li class="list-group-item list-group-item-info"><a href="">Thumbs</a></li>
-      <li class="list-group-item"><a href="">Thumbs</a></li>
-      <li class="list-group-item list-group-item-info"><a href="">Languages</a></li>
-      <li class="list-group-item"><a href="">All Languages</a></li>
-      <li class="list-group-item"><a href="">Add Language</a></li>
-      <li class="list-group-item list-group-item-info"><a href="">Settings</a></li>
-      <li class="list-group-item"><a href="">Settings</a></li>
-    </ul>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-dashboard"></i> Dashboard
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Home</a>
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Preferences</a>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-comments"></i> Forums
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">All Forums</a>
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Add Forum</a>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-user"></i> Users
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">All Users</a>
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Add User</a>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-users"></i> User Groups
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">All Groups</a>
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Add Group</a>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-magic"></i> Themes
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Themes</a>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-thumbs-up"></i> Thumbs
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Thumbs</a>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-language"></i> Languages
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Languages</a>
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Add Language</a>
+		</div>
+	</div>
+	<div class="panel panel-default">
+		<div class="panel-heading">
+		<div class="panel-title">
+			<i class="fa fa-gear"></i> Settings
+		</div>
+		</div>
+		<div class="list-group">
+			<a class="list-group-item" href="<?php echo base_url('admin')?>">Settings</a>
+		</div>
+	</div>
     </div>
 </div>
